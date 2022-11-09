a0f5d59ac340eeea6205a04714142aeb43ccd3f5
Daniyar <kulovdaniyar77@gmail.com>
2022-11-09 22:35:17 +0600

    fix: 02 task txt

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,24 +2,23 @@ const fs = require('fs');
 const path = require('path');
 const { stdin, stdout } = process;
 
-const file = path.join(__dirname, 'text.txt');
+const file = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
 stdout.write('Привет, напишите текст: ');
 
-stdin.on('data', (data) =>{
-  
-  if (data.toString().trim() === 'exit') process.exit();
-
-  fs.appendFile(file, 
-    data,
-    (err) => {
-      if (err) console.log(err);
-    }
-  );
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    endFunc();
+  }
+  file.write(data);
 });
+
 
 process.on('SIGINT', () => {
   process.exit();
 });
 
+const endFunc = () => {
+  process.exit();
+}
 process.on('exit', () => stdout.write('БЫБЫ'));
